227b746c2764fef8ff953d1d92d8c42da93e3f6d
eskarpnes <erl1sharpy@gmail.com>
2018-03-01 22:35:11 +0100

    Customer func added

--- src/Customer.java
+++ src/Customer.java
@@ -1,15 +1,22 @@
+import java.util.Random;
+
 /**
  * This class implements a customer, which is used for holding data and update the statistics
  *
  */
 public class Customer {
 
+    private Random random = new Random();
+    private int maxOrder = SushiBar.maxOrder;
+    private int id;
+
     /**
      *  Creates a new Customer.
      *  Each customer should be given a unique ID
      */
     public Customer() {
-        // TODO Implement required functionality
+        this.id = SushiBar.customerCounter.get();
+        SushiBar.customerCounter.increment();
     }
 
 
@@ -17,7 +24,16 @@ public class Customer {
      * Here you should implement the functionality for ordering food as described in the assignment.
      */
     public synchronized void order(){
-        // TODO Implement required functionality
+        int orders = randomIntInRange(0, maxOrder);
+        int takeout = randomIntInRange(0, orders);
+        SushiBar.servedOrders.add(orders);
+        SushiBar.takeawayOrders.add(takeout);
+        SushiBar.totalOrders.add(orders + takeout);
+        try {
+            Thread.sleep(SushiBar.customerWait);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -25,7 +41,12 @@ public class Customer {
      * @return Should return the customerID
      */
     public int getCustomerID() {
-        // TODO Implement required functionality
+        return this.id;
+    }
+
+    private int randomIntInRange(int min, int max) {
+        int randomNum = random.nextInt((max-min) + 1) + min;
+        return randomNum;
     }
 
     // Add more methods as you see fit
