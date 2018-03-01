3d93e6a73187fb83143ee3c13522af5a7b28d78e
eskarpnes <erl1sharpy@gmail.com>
2018-03-01 22:35:50 +0100

    Door without randomizer added

--- src/Door.java
+++ src/Door.java
@@ -1,8 +1,12 @@
+import java.util.Timer;
+
 /**
  * This class implements the Door component of the sushi bar assignment
  * The Door corresponds to the Producer in the producer/consumer problem
  */
 public class Door implements Runnable {
+
+    private final WaitingArea waitingArea;
 
     /**
      * Creates a new Door. Make sure to save the
@@ -10,6 +14,7 @@ public class Door implements Runnable {
      */
     public Door(WaitingArea waitingArea) {
         // TODO Implement required functionality
+        this.waitingArea = waitingArea;
     }
 
     /**
@@ -19,6 +24,24 @@ public class Door implements Runnable {
     @Override
     public void run() {
         // TODO Implement required functionality
+        int frequency = SushiBar.doorWait;
+        while(SushiBar.isOpen) {
+            spawnCustomer();
+            try {
+                Thread.sleep(frequency);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected void spawnCustomer() {
+        Customer customer = new Customer();
+        try {
+            waitingArea.enter(customer);
+        } catch (IllegalStateException e) {
+            System.out.println("No more room in the waiting area.");
+        }
     }
 
     // Add more methods as you see fit
